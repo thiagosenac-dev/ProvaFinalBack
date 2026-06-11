95cd3cbf44fec0d44b321dfcab86e57e276790c3
thiagosenac-dev <thiagodosenac@gmail.com>
2026-06-11 19:09:45 -0300

    Add files via upload

---
+++ ProvaFinalBack/src/main/java/br/com/senac/ProvaFinalBack/ProvaFinalBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.ProvaFinalBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaFinalBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaFinalBackApplication.class, args);
+	}
+
+}
---
+++ ProvaFinalBack/src/test/java/br/com/senac/ProvaFinalBack/ProvaFinalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.ProvaFinalBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaFinalBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
